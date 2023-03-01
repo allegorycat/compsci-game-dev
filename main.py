122e45835a9bf66edb738166c23397b1a33276a7
allegorycat <yanyanbing001@gmail.com>
2023-03-01 10:18:41 -0800

    This is our project initial state 2

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
+smallfont = pygame.font.SysFont('Helvetica', 36)
 
 text_surface_title = test_font.render("TITLE OF THE GAME", True, 'Blue')
-text_surface_by = test_font.render("By Yan", True, 'Blue')
-text_surface_by = pygame.transform.scale(text_surface_by, (60, 20))
+text_surface_by = smallfont.render('By Yan', True, 'Blue')
+# text_surface_by = pygame.transform.scale(text_surface_by, (60, 20))
+start_game_text = smallfont.render('Start', True, 'Blue')
+
+#button class
 
 
 def title_screen():
@@ -22,7 +26,10 @@ while True:
             pygame.quit()
 
             exit()
+        #if event.type == pygame.MOUSEBUTTONDOWN:
+
     title_screen()
+
     # .blit puts one surface onto another
 
     # draw all elements, update everything
