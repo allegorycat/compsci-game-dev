bc27fd9c4de1d68ca1fe95e63afd4138ad14835f
allegorycat <yanyanbing001@gmail.com>
2023-03-02 13:00:41 -0800

    This is our project initial state 3/2

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
-smallfont = pygame.font.SysFont('None', 48)
-tinyfont = pygame.font.SysFont('Helvetica', 26)
-bigfont = pygame.font.SysFont('None', 55)
+small_font = pygame.font.SysFont('None', 48)
+smallest_font = pygame.font.SysFont('Helvetica', 26)
+big_font = pygame.font.SysFont('None', 55)
 
 
 # def
@@ -21,7 +21,7 @@ class Label:
         self.set(text)
 
     def set(self, text):
-        self.text = smallfont.render(text, 1, pygame.Color("Blue"))
+        self.text = small_font.render(text, 1, pygame.Color("Blue"))
         size = w, h = self.text.get_size()
         self.rect = pygame.Rect(self.x, self.y, w, h)
         self.surface = pygame.Surface(size)
@@ -29,8 +29,8 @@ class Label:
 
 # variables
 
-text_surface_title = bigfont.render("TITLE OF THE GAME", True, 'Blue')
-text_surface_by = tinyfont.render('By Yan', True, 'Blue')
+text_surface_title = big_font.render("TITLE OF THE GAME", True, 'Blue')
+text_surface_by = smallest_font.render('By Yan', True, 'Blue')
 # text_surface_by = pygame.transform.scale(text_surface_by, (60, 20))
 # start_game_text = smallfont.render('Start', True, 'Blue')
 
@@ -41,6 +41,11 @@ lab1 = Label("Start", 20, 200)
 def title_screen():
     screen.blit(text_surface_title, (20, 50))
     screen.blit(text_surface_by, (20, 100))
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if pygame.mouse.get_pressed()[0]:
+            mx, my = pygame.mouse.get_pos()
+            if lab1.rect.collidepoint(mx, my):
+                lab1.set("You clicked me")
 
 def loading_screen():
     pass
@@ -51,11 +56,6 @@ while True:
             pygame.quit()
 
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                mx, my = pygame.mouse.get_pos()
-                if lab1.rect.collidepoint(mx, my):
-                    lab1.set("You clicked me")
 
         screen.blit(lab1.surface, (20, 200))
         #if event.type == pygame.MOUSEBUTTONDOWN:
